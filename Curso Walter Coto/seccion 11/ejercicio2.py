a18dd7cbba587ed4315161fdbaacc5cf8d944ae9
Eduardo Arce - from note <edoarcee@gmail.com>
2022-02-23 09:35:31 -0300

    seccion 11 completa

---
+++ Curso Walter Coto/seccion 11/ejercicio2.py
@@ -0,0 +1,24 @@
+# Con el siguiente diccionario, debes crear un programa que pregunte al usuario por un número; el programa debe imprimir el jugador al que hace referencia ese número
+
+dorsales = {
+    1: "Casillas",
+    15: "Ramos",
+    3: "Pique",
+    5: "Puyol",
+    11: "Capdevila",
+    14: "Xabi Alonso",
+    16: "Busquets",
+    8: "Xavi Hernandez",
+    18: "Pedrito",
+    6: "Iniesta",
+    7: "Villa",
+}
+
+dorsal = int(input("Ingrese Numbero a buscar: "))
+
+jugador = dorsales.get(dorsal)
+
+if jugador:
+    print("El dorsal", dorsal, "corresponde a", jugador)
+else:
+    print("numero no encontrado")
